79f3b9f2e058bcf6b0144fbc216b52513d726d9f
luixlacrux <luixlacrux18@gmail.com>
2017-03-14 20:46:10 -0400

    initial commit

---
+++ bs-config.js
@@ -0,0 +1,23 @@
+/*
+ |--------------------------------------------------------------------------
+ | Browser-sync config file
+ |--------------------------------------------------------------------------
+ |
+ | For up-to-date information about the options:
+ |   http://www.browsersync.io/docs/options/
+ |
+ | There are more options than you see here, these are just the ones that are
+ | set internally. See the website for more info.
+ |
+ |
+ */
+module.exports = {
+  ui: false,
+  files: [
+    'public/*.html',
+    'public/**/*.html',
+    'public/css/*.css',
+    'public/js/*.js'
+  ],
+  server: 'public'
+}
